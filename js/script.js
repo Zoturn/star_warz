e795b8922cc900e1c9836b2d1001dff0a84d31fd
Oleksandr <satturrn@gmail.com>
2017-11-03 21:34:29 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,51 +1,61 @@
 $(document).ready(function () {
 
-    $('#logo').addClass('rollOut')
+  $('#logo').addClass('rollOut')
 
 })
+
+let i = 1
 
 let storage = localStorage.getItem('lastname')
 let isChecked = storage === null ? [] : JSON.parse(storage)
 
-for(i=1; i<=87; i++){
+function allPers () {
 
-  var adress='https://swapi.co/api/people/'+i+'/'
+  var address = 'https://swapi.co/api/people/' + i + '/'
 
-  $.ajax({
-    url: adress,
-    success: function(data){
-      var pers = document.createElement('div')
-      var parentElem = document.getElementById('pers_section')
-      var checkbox = document.createElement('input')
+  fetch(address)
+    .then(response => {
+    i++
+    if (response.status !== 200) return false
+  return response.json()
+})
+.then(data => {
+    if (data) {
+      let pers = document.createElement('div')
+      let parentElem = document.getElementById('pers_section')
+      let checkbox = document.createElement('input')
 
       pers.className = 'pers'
-      checkbox.setAttribute("type", "checkbox")
+      checkbox.setAttribute('type', 'checkbox')
       checkbox.id = data.name
-      apsent = isChecked.indexOf(data.name)
-      if(apsent!== -1){
-        checkbox.setAttribute("checked", "true")
+      absent = isChecked.indexOf(data.name)
+      if (absent !== -1) {
+        checkbox.setAttribute('checked', 'true')
       }
       pers.innerHTML = data.name
       pers.appendChild(checkbox)
       parentElem.appendChild(pers)
-      document.getElementById(data.name).onchange = function(event) {
+      document.getElementById(data.name).onchange = function (event) {
         const id = event.target.id
-        if(document.getElementById(id).checked){
+        if (document.getElementById(id).checked) {
 
           isChecked.push(id)
           localStorage.setItem('lastname', JSON.stringify(isChecked))
-        }else {
+        } else {
           var index = isChecked.indexOf(id)
           isChecked.splice(index, 1)
           localStorage.setItem('lastname', JSON.stringify(isChecked))
         }
       }
     }
-  })
+    if (i <= 88) return allPers()
+})
+.catch(e => {
+    console.log(e)
+})
 }
 
-
-
+allPers()
 
 
 
